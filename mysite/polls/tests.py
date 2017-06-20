0619a2a1e2b67a0543cdb922c2cfcbb368a369a2
Elias Benkhodja <elias.benkhodja@gmail.com>
2017-06-20 10:52:04 +0300

    Use py.test instead of django tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,22 +1,22 @@
 import datetime
+import pytest
 
 from django.utils import timezone
-from django.test import TestCase
 from django.urls import reverse
 
 from .models import Question
 
+pytestmark = pytest.mark.django_db
 
-class QuestionModelTests(TestCase):
+
+class TestQuestionModel:
 
     def test_was_published_recently_with_future_question(self):
-        """
-        was_pulished_recently() must return False for questions whose pub_date
-        is in the future
-        """
+        # was_pulished_recently() must return False for questions whose pub
+        # date is in the future
         time = timezone.now() + datetime.timedelta(days=1)
         future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
+        assert future_question.was_published_recently() is False
 
 
 def create_question(question_text, days):
@@ -25,41 +25,40 @@ def create_question(question_text, days):
         question_text=question_text, pub_date=time)
 
 
-class QuestionIndexViewTests(TestCase):
-    def test_no_questions(self):
-        response = self.client.get(reverse("polls:index"))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+class TestQuestionIndexView:
+    def test_no_questions(self, client):
+        response = client.get(reverse("polls:index"))
+        assert response.status_code == 200
+        assert "No polls are available." in str(response.render().content)
+        assert len(response.context["latest_question_list"]) == 0
 
-    def test_past_question(self):
+    def test_past_question(self, client):
         create_question(question_text="Past question", days=-30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerysetEqual(
-            response.context["latest_question_list"],
-            ["<Question: Past question>"]
-        )
+        response = client.get(reverse("polls:index"))
+        assert repr(response.context["latest_question_list"][0]) \
+            == "<Question: Past question>"
+        assert len(response.context["latest_question_list"]) == 1
 
-    def test_future_question(self):
+    def test_future_question(self, client):
         """Ensure future questions are hidden from view"""
         create_question(question_text="Future question", days=30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+        response = client.get(reverse("polls:index"))
+        assert "No polls are available." in str(response.render().content)
+        assert len(response.context["latest_question_list"]) == 0
 
 
-class QuetionDetailViewTests(TestCase):
-    def test_future_question(self):
+class TestQuestionDetailView:
+    def test_future_question(self, client):
         """Ensure future questions are hidden from view"""
         future_question = create_question(question_text="Future question",
                                           days=5)
         url = reverse("polls:detail", args=(future_question.id,))
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
+        response = client.get(url)
+        assert response.status_code == 404
 
-    def test_past_question(self):
+    def test_past_question(self, client):
         """Ensure other questions show"""
         past_question = create_question(question_text="Past question", days=-5)
         url = reverse("polls:detail", args=(past_question.id,))
-        response = self.client.get(url)
-        self.assertContains(response, past_question.question_text)
+        response = client.get(url)
+        assert past_question.question_text in str(response.render().content)
