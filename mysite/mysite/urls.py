b985f8f3a36390e7edce3f89b5a6e247dc42a48b
Elias Benkhodja <elias.benkhodja@gmail.com>
2017-06-20 11:06:41 +0300

    Add autogenerated swagger spec

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
 
 from polls.api import polls
 
@@ -9,6 +10,7 @@ router.register(r"question", polls.QuestionViewSet)
 
 urlpatterns = [
     url(r"^api/", include(router.urls)),
+    url(r"^api-swagger/", get_swagger_view(title="Polls API")),
     url(r"^polls/", include("polls.urls")),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls)
 ]
